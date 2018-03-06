9f9c6c69fc154dfb57ea87d2a9098cb06292df44
toruchan <ishikawatoru01@gmail.com>
2018-03-06 18:46:57 +0900

    fullPage.jsのライブラリを読み込む修正

---
+++ js/contents.js
@@ -0,0 +1,9 @@
+$(function(){
+$('#fullpage').fullpage({
+    sectionsColor: ['#f2f2f2', '#FCCDA5', '#BAE1FD'],
+    anchors: ['firstPage', 'secondPage', 'thirdPage'],
+    menu: '#nav',
+    navigation: true,
+    scrollBar: true,
+});
+})
